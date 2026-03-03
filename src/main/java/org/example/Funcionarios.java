058dd3b7cb3f68ae0b924f43d9f64e7ab821be77
Leonardo <la601807@gmail.com>
2026-03-03 17:30:21 -0300

    Adicionando a classe Funcionarios (Classe principal)

---
+++ src/main/java/org/example/Funcionarios.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public abstract class Funcionarios  {
+
+    private String nome_funcionario;
+    private String cpf;
+    private double salario;
+
+    public String getNome_funcionario() {
+        return nome_funcionario;
+    }
+
+    public void setNome_funcionario(String nome_funcionario) {
+        this.nome_funcionario = nome_funcionario;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+    public double salarioFinal() {
+        return 0;
+    }
+}
